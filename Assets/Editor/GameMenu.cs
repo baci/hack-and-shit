340050f2db202b5faa6db153f15f6b5f4b0a2a45
pdekhla@gmail.com <pdekhla@gmail.com@98eddaf8-66fe-2f31-d343-093d73b2fe84>
2014-02-15 20:03:19 +0000

    - Fixed bug with player corners creating files. - Fork pieces working

--- Assets/Editor/GameMenu.cs
+++ Assets/Editor/GameMenu.cs
@@ -9,6 +9,7 @@ public class GameMenu : MonoBehaviour
         var game = Selection.activeTransform.gameObject.GetComponent<Game>();
         var cornerPiece = game.corner90prefab;
         var straightPiece = game.straightPrefab;
+        var forkPiece = game.forkPrefab;
 
         Vector2 boardSize = game.BoardSize;
         Vector2 topCorner = new Vector2(-(boardSize.x - 1) * 0.5f, -(boardSize.y - 1) * 0.5f);
@@ -24,7 +25,12 @@ public class GameMenu : MonoBehaviour
                 if (y == (int)topCorner.y && Mathf.Abs(x) > boardSize.x * 0.5 - 2) continue;
                 if (y == topCorner.y + boardSize.y - 1 && Mathf.Abs(x) > boardSize.x * 0.5 - 2) continue;
 
-                var newNode = PrefabUtility.InstantiatePrefab(Random.Range(0, 4)==0 ? straightPiece : cornerPiece) as GameObject;
+                GameObject nodePrefab;
+                int rand = Random.Range(0, 5);
+                if (rand == 0) nodePrefab = straightPiece;
+                else if (rand == 1) nodePrefab = forkPiece;
+                else nodePrefab = cornerPiece;
+                var newNode = PrefabUtility.InstantiatePrefab(nodePrefab) as GameObject;
                 newNode.transform.parent = game.transform;
                 newNode.transform.position = new Vector3(x * 2, y * 2, 0);
 
