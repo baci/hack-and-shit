bfc3221e99148aeac0a0969bc585c01b08ace886
vincependrell@gmail.com <vincependrell@gmail.com@98eddaf8-66fe-2f31-d343-093d73b2fe84>
2014-02-15 20:14:21 +0000

    git is better

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+
+	AudioSource musicSource;
+	float basePitch;
+	AudioSource sfxSource;
+
+	private static AudioController _instance;
+	public static AudioController instance{
+		get{
+			if(_instance == null){
+				GameObject o = new GameObject("Audio Controller", typeof(AudioController));
+				DontDestroyOnLoad(o);
+				_instance = o.GetComponent<AudioController>();
+			}
+
+			return _instance;
+		}
+	}
+
+	void Awake () {
+		musicSource = gameObject.AddComponent<AudioSource>();
+		basePitch = musicSource.pitch;
+		sfxSource = gameObject.AddComponent<AudioSource>();
+	}
+
+	public void PlaySfx(AudioClip clip){
+		if(clip == null) return;
+		sfxSource.PlayOneShot(clip);
+	}
+
+	public void PlayMusic(AudioClip music){
+		musicSource.clip = music;
+		musicSource.loop = true;
+		musicSource.pitch = basePitch;
+		musicSource.Play();
+	}
+}
