d02799467df8f43782348157bb7b366b504c7fb8
till.riemer@googlemail.com <till.riemer@googlemail.com@98eddaf8-66fe-2f31-d343-093d73b2fe84>
2014-02-15 18:28:59 +0000

    ADDED: SynchronousNode.cs + scene modified with 4 entities

---
+++ Assets/Scripts/SynchronousNode.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SynchronousNode : MonoBehaviour {
+
+	public List<SynchronousNode> otherNodes;
+
+	private void Start () 
+	{
+		GetComponent<Touchable>().onTouchBegin += OnTouchBegin;
+		GetComponent<Touchable>().onTouchMove += OnTouchMove;
+		GetComponent<Touchable>().onTouchEnd += OnTouchEnd;
+	}
+	
+	private void OnTouchBegin(Touch touch, Vector3 hitPos)
+	{
+		foreach(SynchronousNode node in otherNodes)
+		{
+			node.GetComponent<ConnectionNode>().OnTouchBegin(touch, hitPos);
+		}
+	}
+
+	private void OnTouchMove(Touch touch)
+	{
+		foreach(SynchronousNode node in otherNodes)
+		{
+			node.GetComponent<ConnectionNode>().OnTouchMove(touch);
+		}
+	}
+
+	private void OnTouchEnd(Touch touch)
+	{
+		foreach(SynchronousNode node in otherNodes)
+		{
+			node.GetComponent<ConnectionNode>().OnTouchEnd(touch);
+		}
+	}
+}
