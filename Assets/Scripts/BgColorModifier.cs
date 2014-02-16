a6928ada4a63c46bea6bafb631befeda56b7c798
vincependrell@gmail.com <vincependrell@gmail.com@98eddaf8-66fe-2f31-d343-093d73b2fe84>
2014-02-16 00:09:08 +0000

    BG bro

---
+++ Assets/Scripts/BgColorModifier.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgColorModifier : MonoBehaviour {
+	
+	public Color minColor;
+	public Color maxColor;
+
+	public AnimationCurve blink;
+	public AnimationCurve blinkAcceleration;
+
+	private float _emission;
+	public float emission{
+		get{return _emission;}
+		set{
+			_emission = Mathf.Clamp01(value);
+			Color c = Color.Lerp(minColor, maxColor, _emission);
+			renderer.material.SetColor("_EmisColor", c);
+		}
+	}
+
+	void Update(){
+		float percentage = GameTime.Instance.GetTimePercentage();
+		float bSpeed = blink.Evaluate(Time.time%2);
+		float b = blinkAcceleration.Evaluate(percentage)*bSpeed;
+
+		emission = b;
+	}
+}
---
+++ Assets/Scripts/SpriteRandomizer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteRandomizer : MonoBehaviour {
+
+	public Sprite[] sprites;
+
+	void Start () {
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+		sr.sprite = sprites[Random.Range(0, sprites.Length)];
+	}
+}
