ee84bb1e79dfc5bf1726458ee45d7afd262b6a09
pdekhla@gmail.com <pdekhla@gmail.com@98eddaf8-66fe-2f31-d343-093d73b2fe84>
2014-02-16 13:10:06 +0000

    - Added 2P mode!

--- Assets/Scripts/GameTitleMenu.cs
+++ Assets/Scripts/GameTitleMenu.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class GameTitleMenu : MonoBehaviour
 {
     public GameObject creditsButton;
+    public GameObject loadMapButton;
+    public int levelToLoad = 1;
 
 	private void Start()
 	{
         GetComponent<Touchable>().onTouchBegin += OnTouchDown;
         creditsButton.GetComponent<Touchable>().onTouchBegin += OnCreditsButtonDown;
+        loadMapButton.GetComponent<Touchable>().onTouchBegin += OnLoadMapButtonDown;
 	}
 
 
@@ -23,14 +26,21 @@ public class GameTitleMenu : MonoBehaviour
         StartCoroutine(DoFadeout(Game.State.CREDITS));
     }
 
+    private void OnLoadMapButtonDown(Touch aTouch, Vector3 aHitPos)
+    {
+        Application.LoadLevel(levelToLoad);
+    }
+
 	private IEnumerator DoFadeout(Game.State aState)
 	{
         iTween.ColorTo(gameObject, new Color(1, 1, 1, 0), 0.5f);
         iTween.ColorTo(creditsButton, new Color(1, 1, 1, 0), 0.5f);
+        iTween.ColorTo(loadMapButton, new Color(1, 1, 1, 0), 0.5f);
 		yield return new WaitForSeconds(0.5f);
         Game.Instance.ChangeState(aState);
         gameObject.SetActive(false);
         creditsButton.SetActive(false);
+        loadMapButton.SetActive(false);
 	}
 }
 
